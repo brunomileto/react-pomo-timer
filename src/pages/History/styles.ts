fe2aea180cce3ae5cfa029ad3e7fd403978de625
bruno.mileto <brunomileto@outlook.com>
2022-09-04 20:14:13 -0300

    Finish status of history table component

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -60,3 +60,27 @@ export const HistoryList = styled.div`
     }
   }
 `;
+
+const STATUS_COLORS = {
+  yellow: "yellow500",
+  green: "green500",
+  red: "red500",
+} as const;
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS;
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: "";
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`;
