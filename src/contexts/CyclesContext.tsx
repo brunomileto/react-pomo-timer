06a251d70dd46ee3516f25412e62135b26969e75
bruno.mileto <brunomileto@outlook.com>
2022-09-07 15:37:34 -0300

    Adding local storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useReducer, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 import {
   ActionTypes,
@@ -6,6 +12,8 @@ import {
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
 } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
+import { Detective } from "phosphor-react";
 
 interface CreateCycleData {
   task: string;
@@ -32,16 +40,36 @@ interface CyclesContextProviderPorps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderPorps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  }); //useState<Cycle[]>([]);
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJson = localStorage.getItem(
+        "@pomo-timer:cycles-state-1.0."
+      );
+      if (storedStateAsJson) {
+        return JSON.parse(storedStateAsJson);
+      }
+    }
+  ); //useState<Cycle[]>([]);
   //const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-
   const { cycles, activeCycleId } = cyclesState;
-
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJson = JSON.stringify(cyclesState);
+    localStorage.setItem("@pomo-timer:cycles-state-1.0.0", stateJson);
+  });
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds);
